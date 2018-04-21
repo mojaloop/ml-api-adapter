a0e98a7931c9a3645abc2b2a8959726b5319da1a
Murthy Kakarlamudi <murthy@modusbox.com>
2018-04-21 18:33:20 -0400

    updated urlparser

--- src/lib/urlparser.js
+++ src/lib/urlparser.js
@@ -1,35 +1,20 @@
-/*****
- License
- --------------
- Copyright © 2017 Bill & Melinda Gates Foundation
- The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- Contributors
- --------------
- This is the official list of the Mojaloop project contributors for this file.
- Names of the original copyright holders (individuals or organizations)
- should be listed with a '*' in the first column. People who have
- contributed from an organization can be listed under the organization
- that actually holds the copyright for their contributions (see the
- Gates Foundation organization for an example). Those individuals should have
- their names indented and be marked with a '-'. Email address can be added
- optionally within square brackets <email>.
- * Gates Foundation
- - Name Surname <name.surname@gatesfoundation.com>
-
- --------------
- ******/
-
 'use strict'
 
 const P = require('bluebird')
 const Config = require('./config')
 
-// const accountRegex = new RegExp(`${Config.HOSTNAME}/accounts/([A-Za-z0-9_]*)/?`, 'i')
-const transfersValidationRegex = new RegExp(`${Config.HOSTNAME}/transfers/([a-f\\d]{8}(-[a-f\\d]{4}){3}-[a-f\\d]{12})`, 'i')
-const accountRegex = new RegExp(`[^/]+(?=/$|$)`, 'i')
+const accountRegex = new RegExp(`${Config.HOSTNAME}/accounts/([A-Za-z0-9_]*)/?`, 'i')
+const transfersRegex = new RegExp(`${Config.HOSTNAME}/transfers/([a-f\\d]{8}(-[a-f\\d]{4}){3}-[a-f\\d]{12})`, 'i')
 const accountsTransfersRouteRegex = new RegExp(/\/accounts\/([A-Za-z0-9_]*)\/transfers/, 'i')
+
+const nameFromAccountUri = (uri, callback) => {
+  const matches = uri.match(accountRegex)
+  const hasCallback = (typeof callback === 'function')
+  if (matches) {
+    return (hasCallback) ? callback(null, matches[1]) : matches[1]
+  }
+  return (hasCallback) ? callback('no match', null) : null
+}
 
 const accountNameFromTransfersRoute = (url) => {
   return new P((resolve, reject) => {
@@ -42,8 +27,18 @@ const accountNameFromTransfersRoute = (url) => {
   })
 }
 
+const idFromTransferUri = (uri, callback) => {
+  const matches = uri.match(transfersRegex)
+  const hasCallback = (typeof callback === 'function')
+  if (matches) {
+    return hasCallback ? callback(null, matches[1]) : matches[1]
+  } else {
+    return hasCallback ? callback('no match', null) : null
+  }
+}
+
 const toTransferUri = (id) => {
-  const matches = id.match(transfersValidationRegex)
+  const matches = id.match(transfersRegex)
   return (matches ? id : `${Config.HOSTNAME}/transfers/${id}`)
 }
 
@@ -54,6 +49,8 @@ const toAccountUri = (name) => {
 
 module.exports = {
   accountNameFromTransfersRoute,
+  nameFromAccountUri,
+  idFromTransferUri,
   toTransferUri,
   toAccountUri
 }
