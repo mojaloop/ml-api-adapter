e7a79d35146da6627a9273c4bdebf0f1e486631b
Unknown <lazlucas@deloitte.co.za>
2018-06-06 13:04:56 +0200

    Fixed integration tests

--- test/integration/domain/transfer/index.test.js
+++ test/integration/domain/transfer/index.test.js
@@ -5,15 +5,14 @@ const Test = require('tape')
 const TransferService = require(`${src}/domain/transfer`)
 const Fixtures = require('../../../fixtures')
 
-Test('transfer service', function (modelTest) {
-  modelTest.test('prepare should', function (prepareTest) {
-    prepareTest.test('prepare a transfer', function (assert) {
+Test('transfer service', (modelTest) => {
+  modelTest.test('prepare should', (prepareTest) => {
+    prepareTest.test('prepare a transfer', async (assert) => {
       let transfer = Fixtures.buildTransfer(Fixtures.generateTransferId())
-      TransferService.prepare({}, transfer).then(result => {
-        assert.equal(result, true)
-        assert.pass()
-        assert.end()
-      })
+      const result = await TransferService.prepare({}, transfer)
+      assert.equal(result, true)
+      assert.pass()
+      assert.end()
     })
 
     prepareTest.end()
