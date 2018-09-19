ec3a2c0eae0a1d918e103200acce8a558f54ea14
rmothilal <rajivmothilal@gmail.com>
2018-09-19 17:29:24 +0200

    fix for broken integration tests

--- test/integration/handlers/notification/index.test.js
+++ test/integration/handlers/notification/index.test.js
@@ -362,7 +362,7 @@ Test('Notification Handler', notificationHandlerTest => {
 
       await Kafka.Producer.produceMessage(messageProtocol, topicConfig, kafkaConfig)
 
-      const operation = 'put'
+      const operation = 'error'
       let responseFrom = await getNotifications(messageProtocol.from, operation, transferId)
       let responseTo = await getNotifications(messageProtocol.to, operation, transferId)
       let currentAttempts = 0
@@ -425,7 +425,7 @@ Test('Notification Handler', notificationHandlerTest => {
 
       await Kafka.Producer.produceMessage(messageProtocol, topicConfig, kafkaConfig)
 
-      const operation = 'put'
+      const operation = 'error'
       let response = await getNotifications(messageProtocol.from, operation, transferId)
       let currentAttempts = 0
       while (!response && currentAttempts < (timeoutAttempts * callbackWaitSeconds)) {
