c8923ffdb1c71fae7668707e35a5ad3053574c19
Shashi <shashikant.hirugade@softwaregroup.com>
2018-09-24 01:01:51 -0500

    removed unused helper.js

--- src/handlers/register.js
+++ src/handlers/register.js
@@ -39,7 +39,7 @@ const NotificationHandler = require('./notification')
  * in each of them. Every handler in the sub-folders must have a registerAllHandlers() function
  * @returns {boolean} - Returns a boolean: true if successful, or throws and error if failed
  */
-const registerAllHandlers = async () => {
+const registerAllHandlers = async (request, h) => {
   return await NotificationHandler.startConsumer()
 }
 
