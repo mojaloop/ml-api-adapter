86c0302189f1a300083560d634ad11d7e5f85fa2
bothadeon <deon.botha@modusbox.com>
2018-10-08 14:37:27 +0200

    Added user friendly error messages to transfer fulfill

--- src/api/transfers/routes.js
+++ src/api/transfers/routes.js
@@ -105,13 +105,13 @@ module.exports = [{
         id: Joi.string().required().description('path')
       },
       payload: {
-        fulfilment: Joi.string().regex(/^[A-Za-z0-9-_]{43}$/).max(48).description('fulfilment of the transfer'),
-        completedTimestamp: Joi.string().regex(/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:(\.\d{3}))(?:Z|[+-][01]\d:[0-5]\d)$/).description('When the transfer was completed'),
-        transferState: Joi.string().required().valid(transferState).description('State of the transfer'),
+        fulfilment: Joi.string().regex(/^[A-Za-z0-9-_]{43}$/).max(48).description('fulfilment of the transfer').label('@ Invalid transfer fulfilment description. @'),
+        completedTimestamp: Joi.string().regex(/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:(\.\d{3}))(?:Z|[+-][01]\d:[0-5]\d)$/).description('When the transfer was completed').label('@ A valid transfer completion date must be supplied. @'),
+        transferState: Joi.string().required().valid(transferState).description('State of the transfer').label('@ Invalid transfer state given. @'),
         extensionList: Joi.object().keys({
           extension: Joi.array().items(Joi.object().keys({
-            key: Joi.string().required().min(1).max(32).description('Key'),
-            value: Joi.string().required().min(1).max(128).description('Value')
+            key: Joi.string().required().min(1).max(32).description('Key').label('@ Supplied key fails to match the required format. @'),
+            value: Joi.string().required().min(1).max(128).description('Value').label('@ Supplied key value fails to match the required format. @')
           })).required().min(1).max(16).description('extension')
         }).optional().description('Extention list')
       }
