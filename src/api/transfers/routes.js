2e40ea6da7b5443258043b6fe55d75f4e161cefb
bothadeon <deon.botha@modusbox.com>
2018-10-08 12:52:25 +0200

    Feature/#410 added user friendly loi error messages

--- src/api/transfers/routes.js
+++ src/api/transfers/routes.js
@@ -26,7 +26,7 @@ const BaseJoi = require('joi-currency-code')(require('joi'))
 const Extension = require('joi-date-extensions')
 const Joi = BaseJoi.extend(Extension)
 const tags = ['api', 'transfers']
-const transferState = [ 'RECEIVED', 'RESERVED', 'COMMITTED', 'ABORTED', 'SETTLED' ]
+const transferState = ['RECEIVED', 'RESERVED', 'COMMITTED', 'ABORTED', 'SETTLED']
 
 module.exports = [{
   method: 'POST',
@@ -54,25 +54,26 @@ module.exports = [{
         'fspiop-signature': Joi.string().optional(),
         'fspiop-uri': Joi.string().optional(),
         'fspiop-http-method': Joi.string().optional()
-      }).unknown(false).options({ stripUnknown: true }),
+      }).unknown(false).options({stripUnknown: true}),
       payload: {
-        transferId: Joi.string().guid().required().description('Id of transfer'),
-        payeeFsp: Joi.string().required().min(1).max(32).description('Financial Service Provider of Payee'),
-        payerFsp: Joi.string().required().min(1).max(32).description('Financial Service Provider of Payer'),
+        transferId: Joi.string().guid().required().description('Id of transfer').label('@ Transfer Id must be in a valid GUID format. @'),
+        payeeFsp: Joi.string().required().min(1).max(32).description('Financial Service Provider of Payee').label('@ A valid Payee FSP number must be supplied. @'),
+        payerFsp: Joi.string().required().min(1).max(32).description('Financial Service Provider of Payer').label('@ A valid Payer FSP number must be supplied. @'),
         amount: Joi.object().keys({
-          currency: Joi.string().required().currency().description('Currency of the transfer'),
+          currency: Joi.string().required().currency().description('Currency of the transfer').label('@ Currency needs to be a valid ISO 4217 currency code. @'),
           amount: Joi.string().required().regex(/^([0]|([1-9][0-9]{0,17}))([.][0-9]{0,3}[1-9])?$/).description('Amount of the transfer')
-        }).required().description('Amount of the transfer'),
-        ilpPacket: Joi.string().required().regex(/^[A-Za-z0-9-_]+[=]{0,2}$/).min(1).max(32768).description('ilp packet'),
-        condition: Joi.string().required().trim().max(48).regex(/^[A-Za-z0-9-_]{43}$/).description('Condition of transfer'),
-        expiration: Joi.string().required().regex(/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:(\.\d{3}))(?:Z|[+-][01]\d:[0-5]\d)$/).description('When the transfer expires'),
+        }).required().description('Amount of the transfer').label('@ Supplied amount fails to match the required format. @'),
+        ilpPacket: Joi.string().required().regex(/^[A-Za-z0-9-_]+[=]{0,2}$/).min(1).max(32768).description('ilp packet').label('@ Supplied ILPPacket fails to match the required format. @'),
+        condition: Joi.string().required().trim().max(48).regex(/^[A-Za-z0-9-_]{43}$/).description('Condition of transfer').label('@ A valid transfer condition must be supplied. @'),
+        expiration: Joi.string().required().regex(/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:(\.\d{3}))(?:Z|[+-][01]\d:[0-5]\d)$/).description('When the transfer expires').label('@ A valid transfer expiry date must be supplied. @'),
         extensionList: Joi.object().keys({
           extension: Joi.array().items(Joi.object().keys({
-            key: Joi.string().required().min(1).max(32).description('Key'),
-            value: Joi.string().required().min(1).max(128).description('Value')
+            key: Joi.string().required().min(1).max(32).description('Key').label('@ Supplied key fails to match the required format. @'),
+            value: Joi.string().required().min(1).max(128).description('Value').label('@ Supplied key value fails to match the required format. @')
           })).required().min(1).max(16).description('extension')
         }).optional().description('Extention list')
-      }
+      },
+      failAction: (request, h, err) => { throw err }
     }
   }
 },
@@ -83,7 +84,7 @@ module.exports = [{
   options: {
     id: 'transfer_fulfilment',
     tags: tags,
-    // auth: Auth.strategy(),
+      // auth: Auth.strategy(),
     description: 'Fulfil a transfer',
     payload: {
       failAction: 'error'
@@ -99,7 +100,7 @@ module.exports = [{
         'fspiop-signature': Joi.string().optional(),
         'fspiop-uri': Joi.string().optional(),
         'fspiop-http-method': Joi.string().optional()
-      }).unknown(false).options({ stripUnknown: true }),
+      }).unknown(false).options({stripUnknown: true}),
       params: {
         id: Joi.string().required().description('path')
       },
