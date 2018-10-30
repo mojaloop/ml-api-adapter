8ed2952e591de00381c2a71ce15b89e81a258b2f
Miguel de Barros <miguel@debarros.me>
2018-10-30 15:49:23 +0000

    Added contributors to callbacks.js

--- src/handlers/notification/callbacks.js
+++ src/handlers/notification/callbacks.js
@@ -17,6 +17,12 @@
  optionally within square brackets <email>.
  * Gates Foundation
  - Name Surname <name.surname@gatesfoundation.com>
+
+ * ModusBox
+ - Shashikant Hirugade <shashikant.hirugade@modusbox.com>
+ - Rajiv Mothilal <rajiv.mothilal@modusbox.com>
+ - Miguel de Barros <miguel.debarros@modusbox.com>
+
  --------------
  ******/
 
