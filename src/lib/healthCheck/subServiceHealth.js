35b700921b55a870303e5aaadabd7e7800e02d9e
Lewis Daly <lewisdaly@me.com>
2019-06-21 15:36:57 +0800

    Implementing health check for ml-api-adapter

--- src/lib/healthCheck/subServiceHealth.js
+++ src/lib/healthCheck/subServiceHealth.js
@@ -1,13 +1,11 @@
+
 /*****
  License
  --------------
  Copyright © 2017 Bill & Melinda Gates Foundation
  The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
-
  http://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-
  Contributors
  --------------
  This is the official list of the Mojaloop project contributors for this file.
@@ -18,24 +16,45 @@
  Gates Foundation organization for an example). Those individuals should have
  their names indented and be marked with a '-'. Email address can be added
  optionally within square brackets <email>.
-
  * Gates Foundation
  - Name Surname <name.surname@gatesfoundation.com>
 
- * Miguel de Barros <miguel.debarros@modusbox.com>
- - Lewis Daly <lewis@vesselstech.com>
-
+ * Lewis Daly <lewis@vesselstech.com>
  --------------
  ******/
-
 'use strict'
 
-const Handler = require('./handler')
+// const mongoose = require('mongoose')
+const { statusEnum, serviceName } = require('@mojaloop/central-services-shared').HealthCheck.HealthCheckEnums
+const Logger = require('@mojaloop/central-services-shared').Logger
 
-module.exports = [
-  {
-    method: 'GET',
-    path: '/health',
-    handler: Handler.getHealth
+const MigrationLockModel = require('../../models/misc/migrationLock')
+
+/**
+ * @function getSubServiceHealthBroker
+ *
+ * @description Gets the health for the broker
+ * //TODO: can we actually get a healthCheck for a producer?
+ * There is no getMetadata method for a producer
+ * @returns Promise<SubServiceHealth> The SubService health object for the broker
+ */
+const getSubServiceHealthBroker = async () => {
+  //TODO: Health check for the Producer?
+  // const consumerTopics = Kafka.Consumer.getListOfTopics()
+  let status = statusEnum.OK
+  // try {
+  //   await Promise.all(consumerTopics.map(t => Kafka.Consumer.isConsumerConnected(t)))
+  // } catch (err) {
+  //   Logger.debug(`getSubServiceHealthBroker failed with error ${err.message}.`)
+  //   status = statusEnum.DOWN
+  // }
+
+  return {
+    name: serviceName.broker,
+    status
   }
-]
+}
+
+module.exports = {
+  getSubServiceHealthBroker
+}
