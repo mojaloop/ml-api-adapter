8369abf6942bab23c12b528ec692ea05b271998a
Lewis Daly <lewisdaly@me.com>
2019-06-22 21:13:50 +0800

    add tests for handler

--- test/unit/handlers/api/handler.test.js
+++ test/unit/handlers/api/handler.test.js
@@ -1,17 +1,112 @@
 'use strict'
 
 const Test = require('tapes')(require('tape'))
+const Sinon = require('sinon')
+const HealthCheck = require('@mojaloop/central-services-shared').HealthCheck.HealthCheck
+
+const Handler = require('../../../../src/api/metadata/handler')
+
+function createRequest (routes) {
+  let value = routes || []
+  return {
+    server: {
+      table: () => {
+        return [{ table: value }]
+      }
+    }
+  }
+}
+
+/**
+ * unwrapResponse
+ *
+ * Use this function to unwrap the innner response body and code from an async Handler
+ */
+const unwrapResponse = async (asyncFunction) => {
+  let responseBody
+  let responseCode
+  const nestedReply = {
+    response: (response) => {
+      responseBody = response
+      return {
+        code: statusCode => {
+          responseCode = statusCode
+        }
+      }
+    }
+  }
+  await asyncFunction(nestedReply)
+
+  return {
+    responseBody,
+    responseCode
+  }
+}
 
 Test('route handler', (handlerTest) => {
+  let sandbox
+
   handlerTest.beforeEach(t => {
+    sandbox = Sinon.createSandbox()
+    sandbox.stub(HealthCheck.prototype, 'getHealth').resolves()
+
     t.end()
   })
 
   handlerTest.afterEach(t => {
+    sandbox.restore()
+
     t.end()
   })
 
-  // TODO: copy tests across from metadata.handler.test
+  handlerTest.test('/health should', healthTest => {
+    healthTest.test('returns the correct response when the health check is up', async test => {
+      // Arrange
+      HealthCheck.prototype.getHealth.resolves({ status: 'OK' })
+      const expectedResponseCode = 200
+
+      // Act
+      const {
+        responseCode
+      } = await unwrapResponse((reply) => Handler.getHealth(createRequest({}), reply))
+
+      // Assert
+      test.deepEqual(responseCode, expectedResponseCode, 'The response code matches')
+      test.end()
+    })
+
+    healthTest.test('returns the correct response when the health check is down', async test => {
+      // Arrange
+      HealthCheck.prototype.getHealth.throws(new Error('getHealth() failed'))
+      const expectedResponseCode = 502
+
+      // Act
+      const {
+        responseCode
+      } = await unwrapResponse((reply) => Handler.getHealth(createRequest({ query: { detailed: true } }), reply))
+
+      // Assert
+      test.deepEqual(responseCode, expectedResponseCode, 'The response code matches')
+      test.end()
+    })
+
+    healthTest.test('is down when there is no response body', async test => {
+      // Arrange
+      HealthCheck.prototype.getHealth.resolves(undefined)
+      const expectedResponseCode = 502
+
+      // Act
+      const {
+        responseCode
+      } = await unwrapResponse((reply) => Handler.getHealth(createRequest({ query: { detailed: true } }), reply))
+
+      // Assert
+      test.deepEqual(responseCode, expectedResponseCode, 'The response code matches')
+      test.end()
+    })
+
+    healthTest.end()
+  })
 
   handlerTest.end()
 })
