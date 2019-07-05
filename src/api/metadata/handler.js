46b1fc8e9f9094fe097cfaac0cdc8305a431d007
Neal Donnan <neal.donnan@modusbox.com>
2019-07-05 14:09:51 +0200

    Fix for metadata API endpoint

--- src/api/metadata/handler.js
+++ src/api/metadata/handler.js
@@ -19,7 +19,7 @@ const Config = require('../../lib/config')
 
 const extractUrls = (request) => {
   const urls = {}
-  request.server.table()[0].table.filter(route => {
+  request.server.table().filter(route => {
     return route.settings.id !== undefined &&
       Array.isArray(route.settings.tags) &&
       route.settings.tags.indexOf('api') >= 0
