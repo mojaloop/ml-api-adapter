6a84a5e43e3f9b9b2c117ad8f52269f275acdf6a
Kalin Krustev <kalin.krustev@gmail.com>
2024-07-03 10:08:41 +0000

    test: fix scripts

--- test/scripts/test-integration.sh
+++ test/scripts/test-integration.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+set -euxo pipefail
 
 docker load -i /tmp/docker-image.tar
 docker-compose up -d
 docker-compose ps
 
 npm run wait-4-docker
+curl localhost:3000/health
 
-curl localhost:3000/health && npm run test:xint | tee ./test/results/test-int.log
+INTEGRATION_TEST_EXIT_CODE=0
+npm run test:xint || INTEGRATION_TEST_EXIT_CODE="$?"
+echo "==> integration tests exited with code: $INTEGRATION_TEST_EXIT_CODE"
+
 docker-compose down
+exit $INTEGRATION_TEST_EXIT_CODE
