ef25628e74694f1400d2e7b76719e51568497c83
Kevin Leyow <kleyow@gmail.com>
2024-08-27 07:57:33 -0500

    add notification for fx-forwarded errors

--- src/handlers/notification/index.js
+++ src/handlers/notification/index.js
@@ -598,7 +598,7 @@ const processMessage = async (msg, span) => {
     return true
   }
 
-  if ([Action.TIMEOUT_RESERVED, Action.FX_TIMEOUT_RESERVED, Action.FORWARDED].includes(action)) {
+  if ([Action.TIMEOUT_RESERVED, Action.FX_TIMEOUT_RESERVED, Action.FORWARDED, Action.FX_FORWARDED].includes(action)) {
     const callbackURLTo = await getEndpointFn(destination, REQUEST_TYPE.PUT_ERROR)
     const endpointTemplate = getEndpointTemplate(REQUEST_TYPE.PUT_ERROR)
     jwsSigner = getJWSSigner(Config.HUB_NAME)
