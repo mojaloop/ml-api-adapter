16db1b015b9733420c18b6f437a51d8d89e3f715
geka.evk <geka.evk@gmail.com>
2025-12-15 21:34:48 +0000

    fix(csi-1997): used isHealthy method for consumer

--- src/handlers/notification/index.js
+++ src/handlers/notification/index.js
@@ -1035,6 +1035,7 @@ const isConnected = () => {
   return notificationConsumer.isConnected() && (PayloadCache ? PayloadCache.isConnected() : true)
 }
 
+/* istanbul ignore next */
 const isHealthy = async () => {
   return (await notificationConsumer.isHealthy()) && (PayloadCache ? PayloadCache.isConnected() : true)
 }
